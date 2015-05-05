60df3e7463560caa9efd88f13933db0c62b95607
yurko <bregey@gmail.com>
2015-05-05 21:15:57 +0300

    bangs removed

--- backup.rb
+++ backup.rb
@@ -21,26 +21,26 @@ class BackupPinterest
   PINTEREST = URI.parse('https://pinterest.com/')
 
   def initialize
-    @username = ARGV.shift or usage!
-    @email    = ARGV.shift or usage!
-    @password = ARGV.shift or usage!
+    @username = ARGV.shift or usage
+    @email    = ARGV.shift or usage
+    @password = ARGV.shift or usage
     @root     = Pathname ARGV.shift || File.expand_path('..', __FILE__)
   end
  
-  def usage!
+  def usage
     warn "backup username email password [destination]"
     exit!
   end
  
-  def backup!
-    login!
-    get_list_of_boards!
-    get_images!
+  def backup
+    login
+    get_list_of_boards
+    get_images
   end
 
   private
 
-  def login!
+  def login
     puts "Logging in as #{@username} (#{@email})"
     visit PINTEREST + '/login'
     fill_in 'username_or_email', with: @email
@@ -48,7 +48,7 @@ class BackupPinterest
     page.find("[type=submit]").click
   end
 
-  def get_list_of_boards!
+  def get_list_of_boards
     visit PINTEREST + @username
     # need to wait for boards loaded
     sleep 5
@@ -66,27 +66,27 @@ class BackupPinterest
     puts "found: #{@boards.size} boards"
   end
 
-  def get_images!
+  def get_images
     @boards.each do |board|
       print "[#{board[:name]}] ".green + "found "
       visit PINTEREST + board[:path]
       sleep 3
 
-      image_urls = get_unique_links!
+      image_urls = get_unique_links
 
       # change links to big size images of max width 736px 
       board[:image_urls] = image_urls.map! {|link| link.gsub('236x', '736x')}
       puts "#{board[:image_urls].size} images"
 
-      download_images! board
+      download_images board
     end
   end
 
-  def get_unique_links!
+  def get_unique_links
     image_links = []
 
     until scrolled_to_bottom?
-      load_all_content!
+      load_all_content
 
       image_links_chunk = js <<-JS
         $('.pinUiImage img').map(function(){ return $(this).attr('src') });
@@ -98,7 +98,7 @@ class BackupPinterest
     image_links.uniq
   end
 
-  def download_images! board
+  def download_images board
     path = @root + 'pinterest.com' + board[:path][1..-1]
     image_urls = board[:image_urls].map(&:to_s).map(&:inspect).join(' ')
     path.mkpath
@@ -109,7 +109,7 @@ class BackupPinterest
     system command
   end
 
-  def load_all_content!
+  def load_all_content
       js %(window.scrollTo(0, 9999999))
       sleep 3
   end
@@ -126,4 +126,4 @@ class BackupPinterest
 
 end
 
-BackupPinterest.new.backup!
+BackupPinterest.new.backup
